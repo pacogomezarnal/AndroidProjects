45188bcb05e823df6f66a80bffdd96e417d166b8
Paco Gomez Arnal <paco.gomez.arnal@gmail.com>
2015-09-26 16:42:19 +0200

    ActivityLife actualizado

--- ActivityLife/app/src/main/java/com/example/dam/activitylife/MainActivity.java
+++ ActivityLife/app/src/main/java/com/example/dam/activitylife/MainActivity.java
@@ -19,6 +19,18 @@ public class MainActivity extends Activity {
         Log.i(TAG, "Se crea el activity Main");
     }
 
+    protected void onResume(){
+        super.onResume();
+
+        Log.i(TAG, "ActivityLife pasa a un segundo plano");
+    }
+
+    protected void onPause(){
+        super.onPause();
+
+        Log.i(TAG, "ActivityLife pasa a primer plano");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
